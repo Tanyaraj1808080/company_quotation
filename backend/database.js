00dc294680b7ae312b4effcf363277773fd4807d
tanya raj <tanyaraj9798731092@gmail.com>
2026-03-06 11:02:48 +0530

    modify  quotation template

--- backend/database.js
+++ backend/database.js
@@ -7,6 +7,7 @@ const sequelize = new Sequelize(
     process.env.DB_PASS,
     {
         host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3307,
         dialect: 'mysql',
         //logging: console.log, // Enable SQL logging to terminal
     }
